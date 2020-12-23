e600e7f50c233211cfe3e599a7d7af4c80c108b7
mcd-50 <ayush.as.shukla@gmail.com>
2020-12-23 23:12:20 +0530

    Fixrs

--- test/component_helper.js
+++
@@ -1,7 +0,0 @@
-const app = require("../dist/app").default;
-const collection_helper = require("../dist/helper/collection_helper").default;
-const constant_helper = require("../dist/helper/constant_helper").default;
-
-exports.app = new app("eae1d7cd404eae17c50eb442cdf495356f364a441a54c88cc7f0df6c85e9e876", "66b047b47ccf8069f8face1d16150010d4273e69af8340a50e1ce749e6271937");
-exports.collection_helper = collection_helper;
-exports.constant_helper = constant_helper;
--- test/index_test.js
+++
@@ -1,5 +0,0 @@
-require("chai").should();
-
-const base_sdk = require("./base_sdk_test");
-
-base_sdk.create_test("lead", { name: "abcd", mobile: { mobile: "1000200010", mobile_code: "91" }, email: "1000200010@test.com", country: "ind" });
